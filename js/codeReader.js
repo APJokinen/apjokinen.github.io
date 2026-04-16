5329c9f19a770d5141dde04b72fdb79b5de19b96
APJokinen <arjokine@students.oamk.fi>
2026-04-16 12:54:23 +0300

    mittarin sarjanumero scanniin

--- js/codeReader.js
+++ js/codeReader.js
@@ -1,11 +1,16 @@
 let codeReader = null;
+const container = document.getElementById("video-container1")
 
-function startScanner() {
+function startScanner1() {
+      
+      container.style.display = "block"
       codeReader = new ZXing.BrowserMultiFormatReader();
 
-      codeReader.decodeFromVideoDevice(null, 'video', (result, err) => {
+      codeReader.decodeFromVideoDevice(null, 'video1', (result, err) => {
         if (result) {
-          console.log("✅ Data: " + result.text + "<br>📦 Tyyppi: " + result.barcodeFormat)
+          //console.log("✅ Data: " + result.text + "<br>📦 Tyyppi: " + result.barcodeFormat)
+          document.getElementById("Mittarin sarjanumero").value = result.text
+          stopScanner1()
         }
 
         if (err && !(err instanceof ZXing.NotFoundException)) {
@@ -14,9 +19,11 @@ function startScanner() {
       });
     }
 
-function stopScanner() {
+function stopScanner1() {
+  container.style.display = "none"
       if (codeReader) {
         codeReader.reset();
+        codeReader = null
         //document.getElementById('result').innerHTML = "⏹️ Skannaus pysäytetty";
       }
     }
