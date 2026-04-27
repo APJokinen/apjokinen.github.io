80be4de74e10edb7698e82287e8491211f302c96
APJokinen <arjokine@students.oamk.fi>
2026-04-27 14:36:05 +0300

    Zoom-luvun pyöristys

--- js/mittari.js
+++ js/mittari.js
@@ -73,7 +73,7 @@ async function zoom(mode){
   if(mode === 'out'){
     if(zoomValue > min){
     zoomValue -= step
-    zoomValue = Math.round(zoomValue)
+    zoomValue = zoomValue.toFixed(1)
     zoomFactor.textContent = zoomValue + " X"
       track.applyConstraints({
         advanced: [{ zoom: zoomValue}]
@@ -82,7 +82,7 @@ async function zoom(mode){
   }else if(mode === 'in'){
       if(zoomValue < max){
       zoomValue += step
-      zoomValue = Math.round(zoomValue)
+      zoomValue = zoomValue.toFixed(1)
       zoomFactor.textContent = zoomValue + " X"
       track.applyConstraints({
         advanced: [{ zoom: zoomValue }]
