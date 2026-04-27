3644b7bd95a9ff7061921b869e63b401ced7bf07
APJokinen <arjokine@students.oamk.fi>
2026-04-27 22:16:15 +0300

    Ocr-kokeilu

--- js/ocrTunnistus.js
+++ js/ocrTunnistus.js
@@ -1,6 +1,7 @@
 let isProcessing = false
 let intervalOn = null
 let OcrStream = null
+let workerArray = []
 const output = document.getElementById('Mittarin_sarjanumero');
 const OcrVideo = document.getElementById('ocr-video')
 const OcrContainer = document.getElementById("ocr-container")
@@ -27,36 +28,66 @@ async function startOcrScanner(){
     }catch(e){
         console.error(e)
     }
+
+    const track = OcrStream.getVideoTracks()[0];
+    const capabilities = track.getCapabilities();
+    if(capabilities?.zoom){
+        const min = capabilities?.zoom?.min
+        const max = capabilities?.zoom?.max
+        document.getElementById("zoomMinCodeOcr").textContent = "Min: "+min
+        document.getElementById("zoomMaxCodeOcr").textContent  = "Max: "+max
+    }
+
     
     for (let i = 0; i < 4; i++) {
-        const worker = await Tesseract.createWorker('fin')
+        const worker = await createWorker();
+        workerArray.push(worker)
+        await worker.setParameters({
+          tessedit_char_whitelist: '0123456789',
+        });
         scheduler.addWorker(worker);
       }
 
-    
-    const oneOcr = async() => {
+}
+
+async function oneOcr() {
         console.log("Yksi freimi")
+        if(isProcessing) return
+        isProcessing = true
         const debug = document.getElementById("ocr-test")
         const canvas = document.createElement("canvas")
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
+        canvas.width = OcrVideo.videoWidth;
+        canvas.height = OcrVideo.videoHeight;
         const ctx = canvas.getContext('2d')
 
+        if (!OcrVideo.videoWidth || !OcrVideo.videoHeight) {
+        console.log("Video ei ole valmis vielä");
+        return;
+    }
         ctx.drawImage(OcrVideo, 0, 0, canvas.width, canvas.height);
+        
+        //const blob = await new Promise(resolve => canvas.toBlob(resolve));
 
+        console.log(OcrVideo.readyState);
+        console.log(OcrVideo.videoWidth, OcrVideo.videoHeight);
+        console.log(canvas.width, canvas.height);
+
+
+        /*if (!blob) {
+        console.log("Blob epäonnistui");
+        return;
+        }*/
 
         const { data: { text } } = await scheduler.addJob('recognize', canvas);
         if(text){
             console.log("Tulos:",text)
-            debug.textContent = text
-            await stopOcrScan()
+            debug.textContent = "Sarjanumero: "+text
         }else{
             console.log("Ei tulosta")
-            debug.textContent = "Ei tulosta"
+            debug.textContent = "Ei sarjanumeroa"
         }
         console.log("Yksi freimi loppui")
-    }
-    intervalOn = setInterval(oneOcr,2000)
+        isProcessing=false
 }
 
 
@@ -65,6 +96,12 @@ async function stopOcrScan(){
     if(intervalOn){
         clearInterval(intervalOn)
     }
+    scheduler.terminate()
+    /*if(workerArray > 0){
+        for(w of workerArray){
+            w.terminate()
+        }
+    }*/
     if(OcrStream){
     const tracks = OcrStream.getTracks();
     tracks.forEach(track => track.stop());
@@ -73,3 +110,54 @@ async function stopOcrScan(){
     OcrContainer.style.display = "none"
 }
 
+async function takePicture(){
+    await oneOcr()
+}
+
+async function lampButtonOcr(){
+    const track = OcrStream.getVideoTracks()[0];
+    const capabilities = track.getCapabilities();
+    if(capabilities?.torch){
+        torchOn = !torchOn
+        await controls.switchTorch(torchOn)
+        
+      if(torchOn){
+        document.getElementById("lightOff").style.display = "block"
+        document.getElementById("lightOn").style.display = "none"
+      }else{
+        document.getElementById("lightOn").style.display = "block"
+        document.getElementById("lightOff").style.display = "none"
+      }
+    }
+}
+
+async function zoomOcr(mode){
+    const track = OcrStream.getVideoTracks()[0];
+    const capabilities = track.getCapabilities();
+
+    if(capabilities?.zoom){
+      const step = 0.2
+
+  if(mode === 'out'){
+    if(zoomValue > zoomMin){
+    zoomValue -= step
+    zoomValue = Math.round((zoomValue * 10))/10
+    zoomFactor.textContent = zoomValue + " X"
+      await track.applyConstraints({
+        advanced: [{ zoom: zoomValue}]
+      });
+      }
+  }else if(mode === 'in'){
+      if(zoomValue < zoomMax){
+      zoomValue += step
+      zoomValue = Math.round((zoomValue * 10))/10
+      zoomFactor.textContent = zoomValue + " X"
+      await track.applyConstraints({
+        advanced: [{ zoom: zoomValue }]
+      });
+      }
+  }
+    }else{
+      console.log("Zoom ei mahdollista tällä laitteella")
+    }
+}
